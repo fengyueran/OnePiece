4cfe0186b1ebbecf17f71e2a8e0357ce2c806b39
xinghunm <xinghunm@curacloudcorp.com>
2018-10-19 14:51:07 +0800

    add @xinghunm/eslint-config package

---
+++ packages/xh-eslint-config/README.md
@@ -0,0 +1,1 @@
+eslint extend airbnb
---
+++ packages/xh-eslint-config/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'eslint-config-airbnb',
+  ].map(require.resolve),
+  rules: {
+    "quotes": 0,
+    "comma-dangle": "off",
+    "no-param-reassign": "off",
+    "react/forbid-prop-types": "off",
+    "no-console": "off",
+    "eol-last": "off",
+    "no-plusplus": "off",
+    "no-unused-vars": "warn",
+    "no-trailing-spaces": "off",
+    "prefer-destructuring": "off",
+    "import/prefer-default-export": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-one-expression-per-line": "off",
+    "react/prefer-stateless-function": "off",
+  },
+  "env": {
+    "es6": true,
+    "node": true,
+    "mocha": true,
+    "browser": true,
+    "jquery": true,
+    "jasmine": true
+  }
+};
