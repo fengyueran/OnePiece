b8849670162d38769946ce9d5ef5c34bea577185
xinghunm <xinghunm@curacloudcorp.com>
2018-11-22 14:45:35 +0800

    style button to test

--- packages/xh-widgets/src/app.js
+++ packages/xh-widgets/src/app.js
@@ -1,15 +1,19 @@
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import Button from './Button';
 
+const StyledButton = styled(Button)`
+  background: #536DFE;
+`;
 class App extends Component {
   render() {
     return (
       <div style={{ padding: 20 }}>
-        <Button>
+        <StyledButton>
           Submit
-        </Button>
+        </StyledButton>
       </div>
     );
   }
