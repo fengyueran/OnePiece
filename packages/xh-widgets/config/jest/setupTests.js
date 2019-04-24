3acde25b9b729f73dd017c68af8e62f377493d6c
xinghunm <xinghunm@curacloudcorp.com>
2019-04-24 17:00:03 +0800

    revise project with new template

--- packages/xh-widgets/config/jest/setupTests.js
+++
@@ -1,5 +0,0 @@
-import 'raf/polyfill'; // fix warning React depends on requestAnimationFrame
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
