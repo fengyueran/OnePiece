e4fc4813a675cfb381d3fdf4a637bd7bb8bf5654
xinghunm <xinghunm@curacloudcorp.com>
2019-11-11 13:30:13 +0800

    update example

--- packages/xh-widgets/src/app.js
+++ packages/xh-widgets/src/app.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
-import Carousel from './Carousel';
-import slice1 from './assets/slice1.png';
-import slice2 from './assets/slice2.png';
-import slice3 from './assets/slice3.png';
+import Modal from './Modal';
 
 const Container = styled.div`
   /* position: absolute;
@@ -12,17 +9,20 @@ const Container = styled.div`
   left: 50%;
   margin-top: 200px; */
   width: 300px;
+  height: 500px;
+  background: blue;
   margin-left: 200px;
 `;
 
-const images = [{ src: slice1 }, { src: slice2 }, { src: slice3 }];
 class App extends Component {
   render() {
     console.log('app*************');
     return (
-      <Container>
-        <Carousel images={images} />
-      </Container>
+      <Container
+        onClick={() => {
+          Modal.confirm();
+        }}
+      />
     );
   }
 }
