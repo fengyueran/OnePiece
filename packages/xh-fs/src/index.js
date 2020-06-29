e9e6906174ae235ec6300b139643d1d229b9c5e0
xinghunm <xinghunm@keyayun.com>
2020-06-29 15:22:00 +0800

    init xh-fs

---
+++ packages/xh-fs/src/index.js
@@ -0,0 +1,1 @@
+export { default as writeJson } from "./write-json";
---
+++ packages/xh-fs/src/write-json.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const writeJson = (json, filePath) =>
+  new Promise((resolve, reject) => {
+    const jsonStr = JSON.stringify(json, null, 2);
+    fs.writeFile(filePath, jsonStr, "utf8", (err) => {
+      if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        reject(err);
+      }
+      console.log("JSON file has been saved.");
+      resolve();
+    });
+  });
+
+export default writeJson;
